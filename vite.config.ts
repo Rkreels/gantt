4598433acf300c8d81e634abdfa95d26a74bb31e
mycampushub <mycampuslifestyle@gmail.com>
2026-03-16 06:33:32 +0600

    gt should work now

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/',
+  base: '/gantt-chart/',
   plugins: [react()],
   resolve: {
     alias: {
